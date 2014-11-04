8bca3ca77de8decfcc51e3e9775764c8d72e2d40
Smita Ghosh <smitag@codeaurora.org>
2014-11-04 15:48:30 -0800

    mdmzirc: Enable support for eMMC in userspace
    mdev script has been modified to support eMMC.
    
    Change-Id: I9bf27cc5e0414257d3b4103a9e8d4493cf07d291

--- recipes/busybox/files/automountsdcard.sh
+++ recipes/busybox/files/automountsdcard.sh
@@ -17,10 +17,38 @@ mount_partition()
         fi
 }
 
+check_if_boot_dev()
+{
+                ret_val=`cat /proc/cmdline | grep "androidboot.bootdevice" |wc -l`
+                if  [ $ret_val -eq 1 ]; then
+                    boot_dev=`cat /proc/cmdline | awk '{ for ( n=1; n<=NF; n++ ) if($n ~ "androidboot.bootdevice") print $n }' | awk '{split($0,a, "=");print a[2]}'`
+                    boot_dev=/sys/devices/$boot_dev
+                    real_sysfs_path=`realpath /sys/class/block/$1`
+                    ret_val=`echo $real_sysfs_path |grep $boot_dev |wc -l`
+                fi
+                echo $ret_val
+}
+
+
+create_symlink()
+{
+                real_sysfs_path=`realpath /sys/class/block/$1`
+                partition_name=`cat $real_sysfs_path/uevent | awk '{ for ( n=1; n<=NF; n++ ) if($n ~ "PARTNAME") print $n }' | awk '{split($0,a, "=");print a[2]}'`
+                mkdir -p /dev/block/bootdevice/by-name/
+                partition_name=/dev/block/bootdevice/by-name/$partition_name
+                target_dev=/dev/$1
+                ln -s $target_dev $partition_name
+}
+
 case "${ACTION}" in
 add|"")
-        umount_partition ${MDEV}
-        mount_partition ${MDEV}
+        result=`check_if_boot_dev $MDEV`
+        if [ $result -eq 1 ]; then
+                 create_symlink $MDEV &
+        else
+                 umount_partition ${MDEV}
+                 mount_partition ${MDEV}
+        fi
         ;;
 remove)
         umount_partition ${MDEV}
